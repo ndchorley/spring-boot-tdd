4ccd5d6b794562c5ae81d7ac079e862681457816
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-01-23 08:53:18 +0000

    Introduce a POST endpoint

--- src/main/java/com/example/springboottdd/Controller.java
+++ src/main/java/com/example/springboottdd/Controller.java
@@ -1,9 +1,13 @@
 package com.example.springboottdd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 public class Controller {
@@ -11,4 +15,8 @@ public class Controller {
     public List<String> root() {
         return List.of();
     }
+
+    @PostMapping("/")
+    @ResponseStatus(CREATED)
+    public void addGame() {}
 }
