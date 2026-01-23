e8dd7caad6f682946821f569cdaac329021a9fb3
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-01-23 09:32:21 +0000

    Refactor to extract the list into a field

--- src/main/java/com/example/springboottdd/Controller.java
+++ src/main/java/com/example/springboottdd/Controller.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
 public class Controller {
+    private final ArrayList<String> games = new ArrayList<>();
+
     @GetMapping("/")
     public List<String> root() {
-        return List.of();
+        return games;
     }
 
     @PostMapping("/")
