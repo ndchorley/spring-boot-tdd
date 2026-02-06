482811144284dc270f4b26430bab970114b53b88
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-02-06 08:12:01 +0000

    Introduce an ApplicationConfiguration class

---
+++ src/main/java/com/example/springboottdd/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.springboottdd;
+
+import com.example.springboottdd.storage.InMemoryRepository;
+import com.example.springboottdd.storage.Repository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ApplicationConfiguration {
+    @Bean
+    public Repository repository() {
+        return new InMemoryRepository();
+    }
+}
