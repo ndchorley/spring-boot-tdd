adbf854009ff94d205b7078fffe23a2fcc647e4a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-02-18 08:17:34 +0000

    Move functional tests to their own package

--- src/test/java/com/example/springboottdd/functional/SpringBootTddApplicationTests.java
+++ src/test/java/com/example/springboottdd/functional/SpringBootTddApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springboottdd;
+package com.example.springboottdd.functional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
